4a778e18a89b5fc08c08bd4424b90534d13ebdd9
Jessie Sidhu <jsidhu22@gmail.com>
2019-05-30 17:12:19 -0700

    Updates

---
+++ precache-manifest.d1b1d33378938eae079b7162387dd250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81380e347d64579f29ee863a084ccd0b",
+    "url": "/billy-shop/index.html"
+  },
+  {
+    "revision": "e6825a0b991c1596da10",
+    "url": "/billy-shop/static/css/main.90b69f39.chunk.css"
+  },
+  {
+    "revision": "7e42ca3564d78b2df63c",
+    "url": "/billy-shop/static/js/2.2b3f9904.chunk.js"
+  },
+  {
+    "revision": "e6825a0b991c1596da10",
+    "url": "/billy-shop/static/js/main.82698eab.chunk.js"
+  },
+  {
+    "revision": "c7ff34775ae90cfbbb7e",
+    "url": "/billy-shop/static/js/runtime~main.a2f7ea4c.js"
+  },
+  {
+    "revision": "2ad468ce9a73c716ec0045c948c3c767",
+    "url": "/billy-shop/static/media/yellow.2ad468ce.jpg"
+  }
+]);
